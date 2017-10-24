756b61f547c7cec03be7d8d388cbf32c13a476d4
Silenceneo-xw <silenceneo_xw@163.com>
2017-10-24 21:22:31 +0800

    加入显示天气信息到界面上的功能。

--- app/src/main/java/com/silenceneoxw/coolweather/util/Utility.java
+++ app/src/main/java/com/silenceneoxw/coolweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.silenceneoxw.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.silenceneoxw.coolweather.db.City;
 import com.silenceneoxw.coolweather.db.County;
 import com.silenceneoxw.coolweather.db.Province;
+import com.silenceneoxw.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -86,5 +88,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的 JSON 数据解析成 Weather 实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
